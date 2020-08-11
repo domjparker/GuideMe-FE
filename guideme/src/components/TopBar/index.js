481018deb9ba59a45abdfee6b5bea3dc5e3921c4
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-08-11 13:45:36 -0700

    merge commit

--- guideme/src/components/TopBar/index.js
+++ guideme/src/components/TopBar/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 
-function Footer(props){
+function TopBar(props){
 
 return (
     <>
@@ -11,4 +11,4 @@ return (
 
 }
 
-export default Footer;
+export default TopBar;
