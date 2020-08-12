026fcd51c4a2547c5ab80109465529c3e9eb5765
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-08-11 23:27:09 -0700

    small additions

--- guideme/src/pages/Adventures/index.js
+++ guideme/src/pages/Adventures/index.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, Lazy, Suspense} from 'react'
 import './style.css'
 import TopBar from '../../components/TopBar'
 import Wrapper from '../../components/Wrapper'
 import Footer from '../../components/Footer'
 import Gridx from '../../components/Gridx'
 import Cell from '../../components/Cell'
-import FlipCard from '../../components/FlipCard'
 import adventureList from '../../fakedb/adventures.json'
+import FlipCard from '../../components/FlipCard'
 
 
 function Adventures(){
