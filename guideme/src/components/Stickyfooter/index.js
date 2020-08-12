e065cc002313f0a06234964ed75e7ba93dc1a743
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-08-11 14:20:36 -1000

    sticky footer with links added.

---
+++ guideme/src/components/Stickyfooter/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './style.css'
+
+
+function Stickyfooter() {
+
+    return (
+        <div className="sticky-footer-css">
+            
+                <div className="columns shrink footer ">
+                    <ul className="menu">
+                        <li><a href="">Link</a></li>
+                        <li><a href="">Link</a></li>
+                        <li><a href="">Link</a></li>
+                    </ul>
+                </div>
+            
+        </div>
+
+
+    )
+}
+
+export default Stickyfooter;
--- guideme/src/pages/Homepage/index.js
+++ guideme/src/pages/Homepage/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './style.css'
-// import TopBar from '../../components/TopBar'
+import TopBar from '../../components/TopBar'
 import Wrapper from '../../components/Wrapper'
+import Footer from '../../components/Footer'
+
+import HomepageImg from '../../images/homepage.jpg'
 
 function Homepage(){
     return (
         <>
-         <img data-interchange="[assets/img/interchange/small.jpg, small], [assets/img/interchange/medium.jpg, medium], [assets/img/interchange/large.jpg, large]" alt="test"></img>
-            {/* <TopBar title={'Adventures'}/> */}
+        
+            <TopBar title={'Adventures'}/>
             <Wrapper>
                 <p>Here be Da Homepage</p>
-               
+                <img className="homebackgrnd"src={HomepageImg} alt="homepagepic"/>
             </Wrapper>
+            <Footer />
         </>
     )
 }
