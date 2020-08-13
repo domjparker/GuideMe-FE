bd7481b76b807abd7813194a538a28c456b4b818
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-08-13 14:04:02 -0700

    user data get util linked in to profile page

--- guideme/src/util/API.js
+++ guideme/src/util/API.js
@@ -1,24 +1,29 @@
 import axios from 'axios'
+const prefix = "http://localhost:3001"
 
 export default {
     //GET all adventures
     getAllAdventures: () => {
-        return axios.get('/api/adventure')
+        return axios.get(`${prefix}/api/adventure`, {withCredentials:true})
     },
     //GET adventures by host id
     getAdventurebyHost: (id) => {
-        return axios.get('/api/adventure/host/' + id)
+        return axios.get(`${prefix}/api/adventure/host/` + id, {withCredentials:true})
     },
     //GET adventures by id
     getAdventurebyId: (id) => {
-        return axios.get('/api/adventure/' + id)
+        return axios.get(`${prefix}/api/adventure/` + id, {withCredentials:true})
     },
     //GET adventures by location
     getAdventurebyLocation: (location) => {
-        return axios.get('/api/adventure/location/' + location)
+        return axios.get(`${prefix}/api/adventure/location/` + location, {withCredentials:true})
     },
     //GET user by id
     getUserbyId: (id) => {
-        return axios.get('/api/user/' + id)
+        return axios.get(`${prefix}/api/user/` + id, {withCredentials:true})
+    },
+    //GET tag by id
+    getTagbyId: (id) => {
+        return axios.get(`${prefix}/api/tag/` + id, {withCredentials:true})
     }
 }
