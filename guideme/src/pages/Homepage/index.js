6f5ae8f890141937fb89a4b6ea2d44ecef837446
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-08-13 11:28:22 -1000

    search added to homepage

--- guideme/src/pages/Homepage/index.js
+++ guideme/src/pages/Homepage/index.js
@@ -9,16 +9,35 @@ function Homepage(props) {
     return (
         <>
             <Wrapper>
-                <div>
-                    <div className="hero-section homepage">
-                        <div className="hero-section-text hpHeader">
-                            <h1 className="hpTitle"><strong>GuideMe </strong></h1>
-                            <button className="button   findAdventure" href="#">Find an Adventure</button>
-                            <button className="button hostAdventure" href="#"> Host an Adventure</button>
+
+                <div className="hero-section homepage">
+                    <h1 className="hpTitle"><strong>GuideMe </strong></h1>
+
+                    <div className="hero-section-text hpHeader">
+                        <div className="callout">
+                            {/* <input className="findAdventure" type="search" placeholder="ie:Hiking" /> */}
+                            <div className="container">
+                                <form className="findAdventure">
+                                    <input
+                                        onChange={props.handleInputChange}
+                                        value={props.value}
+                                        name="search"
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="ie:Hiking"
+                                        id="search"
+                                    />
+                                </form>
+                            </div>
+                            <button className="button searchAdventure" href="#"> Search</button>
+                            {/* <>
+                            <button className="button hostAdventure" href="#"> Host Adventure</button>
+                           </> */}
                         </div>
                     </div>
-                    <div><p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero?  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero??</p></div>
                 </div>
+                <div><p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero?  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero??</p></div>
+
             </Wrapper>
 
         </>
