e89027458d644b5d9da534f37621b0c441a09d20
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-08-13 10:33:12 -0700

    profile page components loaded

--- guideme/src/components/TagRow/index.js
+++ guideme/src/components/TagRow/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './style.css'
 import Cell from '../Cell'
+import Gridx from '../Gridx'
 
 function TagRow(props) {
        const {tags} = props
+       
     return (
         <>
         <Cell size={""}>
-            {/* {tags.map(tag => (<div className="tagDiv" key={props.tags.indexOf(tag)}>{tag}</div>))} */}
+            <Gridx classes={"grid-padding-x align-center"}>
+                {tags? tags.map(tag => (<Cell  size="small-3 medium-2 tagDiv" key={props.tags.indexOf(tag)}>{tag}</Cell>)):null}
+            </Gridx>
         </Cell>
         </>        
     )
--- guideme/src/pages/Profile/index.js
+++ guideme/src/pages/Profile/index.js
@@ -4,6 +4,7 @@ import Wrapper from '../../components/Wrapper'
 import Gridx from '../../components/Gridx'
 import Cell from '../../components/Cell'
 import TagRow from '../../components/TagRow'
+import FlipCard from '../../components/FlipCard'
 
 import usersInfo from '../../fakedb/users.json'
 
@@ -14,12 +15,13 @@ function Profile (props) {
     handlePageChange("Profile")
        
     const loadUserData = () => {
-        setUserData(usersInfo[0])
+        setUserData(usersInfo[1])
     }
     useEffect(() => {
         loadUserData()
     
     }, [])
+
 
     return(
         <>
@@ -42,11 +44,20 @@ function Profile (props) {
                     </Cell>
                 </Gridx>
                 
-                {(userData.host=== false) ? 'Not a host' 
+                {(userData.host=== false) ? null 
                 :(
+                    <>
                 <Gridx classes="grid-margin-x">
-                    {/* <TagRow tags={userData.tags}/> */}
+                    <TagRow tags={userData.tags}/>
                 </Gridx>
+                <Gridx classes="grid-margin-x">
+                    {(userData.hopstedAdventures)? userData.hostedAdventures.map(adventure => (
+                        <Cell key={adventure.id} size={'medium-6 large-4'}>
+                            <FlipCard key={adventure.id} location={adventure.location} difficulty={adventure.difficulty} duration={adventure.duration} maxGroupSize={adventure.maxGroupSize} minGroupSize={adventure.minGroupSize} itinerary={adventure.itinerary} img={adventure.imageId} title={adventure.adventureName} host={adventure.hostId} description={adventure.description}/>
+                        </Cell>
+                    )) : null}
+                </Gridx>
+                </>
                 )}
             </div>
         </Wrapper>
