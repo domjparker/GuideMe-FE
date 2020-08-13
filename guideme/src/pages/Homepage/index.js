7c7da3aa97f62c3ea474029cf47ea1683f3f809a
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-08-12 22:20:37 -1000

    homepage adventure buttons added

--- guideme/src/pages/Homepage/index.js
+++ guideme/src/pages/Homepage/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './style.css'
 import Wrapper from '../../components/Wrapper'
-import Images from '../../images/homepage.jpg'
 import './style.css'
 
 function Homepage(props) {
@@ -13,9 +12,12 @@ function Homepage(props) {
                 <div>
                     <div className="hero-section homepage">
                         <div className="hero-section-text hpHeader">
-                            <h1 className="hpTitle"><strong>GuideME </strong></h1>
+                            <h1 className="hpTitle"><strong>GuideMe </strong></h1>
+                            <button className="button   findAdventure" href="#">Find an Adventure</button>
+                            <button className="button hostAdventure" href="#"> Host an Adventure</button>
                         </div>
                     </div>
+                    <div><p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero?  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, necessitatibus reiciendis laudantium dolore, aperiam aliquid obcaecati optio rerum illum eaque aliquam fugit mollitia nemo quasi magnam accusantium animi sunt libero??</p></div>
                 </div>
             </Wrapper>
 
