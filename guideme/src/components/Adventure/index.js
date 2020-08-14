35ee58111d256c25f9dcb94cfef4b32f903045ed
indyminhas <66227608+indyminhas@users.noreply.github.com>
2020-08-14 12:58:08 -0700

    minor changes

--- guideme/src/components/Adventure/index.js
+++ guideme/src/components/Adventure/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import API from "../util/API";
+import API from "../../util/API";
 import { Input, TextArea, FormBtn } from "../Form";
+import Gridx from '../../components/Gridx'
+import Cell from '../../components/Cell'
+
 
 
 function Adventure() {
@@ -9,7 +12,7 @@ function Adventure() {
     // update the initial state to provide values for
     // the controls in the form (use empty strings)
   
-    const [formObject, setFormObject] = useState({ adventure: '', location: '', price: '', info: ''})
+    const [formObject, setFormObject] = useState({ adventureName: '', hostId: '', usersOnAdventure: '[]', description: '',  location: '', itinerary: '', duration: '', difficulty: '', minGroupSize: '', maxGroupSize: '', price: '', gearList: '', tags: ''})
   
   
   
@@ -23,7 +26,7 @@ function Adventure() {
       event.preventDefault();
        API.postNewAdventure(formObject)
         .then(data => {
-          getAllAdventures();
+          Adventure();
         })
     }
     
@@ -42,17 +45,65 @@ function Adventure() {
           <Cell size="medium-6">
             
             <form>
+            <Input
+                onChange={handleInputChange}
+                name="adventureName"
+                placeholder="Adventure:"
+                value={formObject.adventureName}
+              />
               <Input
                 onChange={handleInputChange}
-                name="adventure"
-                placeholder="Keyword (required)"
-                value={formObject.adventure}
+                name="hostId"
+                placeholder="Host"
+                value={formObject.hostId}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="usersOnAdventure"
+                placeholder="Users"
+                value={formObject.usersOnAdventure}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="description"
+                placeholder="Description:"
+                value={formObject.description}
               />
               <Input
                 onChange={handleInputChange}
                 name="location"
-                placeholder="Location (required)"
+                placeholder="Location:"
                 value={formObject.location}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="itinerary"
+                placeholder="Itinerary:"
+                value={formObject.itinerary}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="duration"
+                placeholder="Duration:"
+                value={formObject.duration}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="difficulty"
+                placeholder= "Difficulty:"
+                value={formObject.difficulty}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="minGroupSize"
+                placeholder="Min. Group Size:"
+                value={formObject.minGroupSize}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="maxGroupSize"
+                placeholder="Max. Group Size:"
+                value={formObject.maxGroupSize}
               />
               <Input
                 onChange={handleInputChange}
@@ -60,9 +111,18 @@ function Adventure() {
                 placeholder="Price:"
                 value={formObject.price}
               />
-              <TextArea name="info" placeholder="Additional Information (Optional)" />
-  
-              
+              <Input
+                onChange={handleInputChange}
+                name="gearList"
+                placeholder="Gear Needed:"
+                value={formObject.gearList}
+              />
+              <Input
+                onChange={handleInputChange}
+                name="tags"
+                placeholder="Tags:"
+                value={formObject.tags}
+              />
               <FormBtn
                 disabled={!(formObject.author && formObject.title)}
                 onClick={handleFormSubmit}
