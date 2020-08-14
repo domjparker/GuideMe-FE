19eff6282ee8ec48cf968c0959910048070c1733
indyminhas <66227608+indyminhas@users.noreply.github.com>
2020-08-14 15:26:49 -0700

    modified components

--- guideme/src/components/Adventure/index.js
+++ guideme/src/components/Adventure/index.js
@@ -1,139 +1,139 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import API from "../../util/API";
-import { Input, TextArea, FormBtn } from "../Form";
+import { Input, FormBtn } from "../Form";
 import Cell from '../Cell'
 import Gridx from '../Gridx'
 
 
 function Adventure() {
-    // Setting our component's initial state
-  
-    // update the initial state to provide values for
-    // the controls in the form (use empty strings)
-  
-    const [formObject, setFormObject] = useState({ adventureName: '', hostId: '', usersOnAdventure: '[]', description: '',  location: '', itinerary: '', duration: '', difficulty: '', minGroupSize: '', maxGroupSize: '', price: '', gearList: '', tags: ''})
-  
-  
-  
-    function handleInputChange(event) {
-      // add code to control the components here
-      setFormObject({ ...formObject, [event.target.name]: event.target.value })
-    }
-  
-    function handleFormSubmit(event) {
-      // add code here to post a new adventure to the api
-      event.preventDefault();
-       API.postNewAdventure(formObject)
-        .then(data => {
-          // getAllAdventures();
-        })
-    }
-    
-    // function deleteAdventure(id) {
-      // add code here to remove a adventures using API
-    //  API.deleteAdventure(id)
-    //   .then(data => {
-    //     loadAdventures();
-    //     setFormObject({adventureName: '', hostId: '', usersOnAdventure: '[]', description: '',  location: '', itinerary: '', duration: '', difficulty: '', minGroupSize: '', maxGroupSize: '', price: '', gearList: '', tags: ''})
-    //   })
-    // }
-    
-    return (
-      <div className="grid-container fluid">
-        <Gridx>
-          <Cell size="medium-6">
-            
-            <form>
-            <Input
-                onChange={handleInputChange}
-                name="adventureName"
-                placeholder="Adventure:"
-                value={formObject.adventureName}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="hostId"
-                placeholder="Host ID"
-                value={formObject.hostId}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="usersOnAdventure"
-                placeholder="Users"
-                value={formObject.usersOnAdventure}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="description"
-                placeholder="Description:"
-                value={formObject.description}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="location"
-                placeholder="Location:"
-                value={formObject.location}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="itinerary"
-                placeholder="Itinerary:"
-                value={formObject.itinerary}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="duration"
-                placeholder="Duration:"
-                value={formObject.duration}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="difficulty"
-                placeholder= "Difficulty:"
-                value={formObject.difficulty}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="minGroupSize"
-                placeholder="Min. Group Size:"
-                value={formObject.minGroupSize}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="maxGroupSize"
-                placeholder="Max. Group Size:"
-                value={formObject.maxGroupSize}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="price"
-                placeholder="Price:"
-                value={formObject.price}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="gearList"
-                placeholder="Gear Need:"
-                value={formObject.gearList}
-              />
-              <Input
-                onChange={handleInputChange}
-                name="tags"
-                placeholder="Tags:"
-                value={formObject.tags}
-              />
-              <FormBtn
-                disabled={!(formObject.author && formObject.title)}
-                onClick={handleFormSubmit}
-              >
-                Search Adventure
-                </FormBtn>
-            </form>
-          </Cell>
-        </Gridx>
-      </div>
-    );
+  // Setting our component's initial state
+
+  // update the initial state to provide values for
+  // the controls in the form (use empty strings)
+
+  const [formObject, setFormObject] = useState({ adventureName: '', hostId: '', usersOnAdventure: '[]', description: '', location: '', itinerary: '', duration: '', difficulty: '', minGroupSize: '', maxGroupSize: '', price: '', gearList: '', tags: '' })
+
+
+
+  function handleInputChange(event) {
+    // add code to control the components here
+    setFormObject({ ...formObject, [event.target.name]: event.target.value })
   }
-  
-  
-  export default Adventure;
+
+  function handleFormSubmit(event) {
+    // add code here to post a new adventure to the api
+    event.preventDefault();
+    API.postNewAdventure(formObject)
+      .then(data => {
+        // getAllAdventures();
+      })
+  }
+
+  // function deleteAdventure(id) {
+  // add code here to remove a adventures using API
+  //  API.deleteAdventure(id)
+  //   .then(data => {
+  //     loadAdventures();
+  //     setFormObject({adventureName: '', hostId: '', usersOnAdventure: '[]', description: '', location: '', itinerary: '', duration: '', difficulty: '', minGroupSize: '', maxGroupSize: '', price: '', gearList: '', tags: ''})
+  //   })
+  // }
+
+  return (
+    <div className="grid-container fluid">
+      <Gridx>
+        <Cell size="medium-6">
+
+          <form>
+            <Input
+              onChange={handleInputChange}
+              name="adventureName"
+              placeholder="Adventure:"
+              value={formObject.adventureName}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="hostId"
+              placeholder="Host ID"
+              value={formObject.hostId}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="usersOnAdventure"
+              placeholder="Users"
+              value={formObject.usersOnAdventure}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="description"
+              placeholder="Description:"
+              value={formObject.description}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="location"
+              placeholder="Location:"
+              value={formObject.location}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="itinerary"
+              placeholder="Itinerary:"
+              value={formObject.itinerary}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="duration"
+              placeholder="Duration:"
+              value={formObject.duration}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="difficulty"
+              placeholder="Difficulty:"
+              value={formObject.difficulty}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="minGroupSize"
+              placeholder="Min. Group Size:"
+              value={formObject.minGroupSize}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="maxGroupSize"
+              placeholder="Max. Group Size:"
+              value={formObject.maxGroupSize}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="price"
+              placeholder="Price:"
+              value={formObject.price}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="gearList"
+              placeholder="Gear Need:"
+              value={formObject.gearList}
+            />
+            <Input
+              onChange={handleInputChange}
+              name="tags"
+              placeholder="Tags:"
+              value={formObject.tags}
+            />
+            <FormBtn
+              disabled={!(formObject.author && formObject.title)}
+              onClick={handleFormSubmit}
+            >
+              Search Adventure
+                </FormBtn>
+          </form>
+        </Cell>
+      </Gridx>
+    </div>
+  );
+}
+
+
+export default Adventure;
--- guideme/src/pages/Adventures/index.js
+++ guideme/src/pages/Adventures/index.js
@@ -5,11 +5,8 @@ import Gridx from '../../components/Gridx'
 import Cell from '../../components/Cell'
 import FlipCard from '../../components/FlipCard'
 import API from '../../util/API'
-<<<<<<< HEAD
-=======
 import{useParams} from 'react-router-dom'
 import Adventure from '../../components/Adventure'
->>>>>>> Dev
 
 
 function Adventures(props){
@@ -34,7 +31,7 @@ function Adventures(props){
                     <Gridx classes={'grid-margin-x'}>
                         {adventures.map(adventure => 
                         <Cell key={adventure.hostId + " " + adventure._id} size={'medium-6 large-4'}>
-                            <FlipCard key={adventure._id} location={adventure.location} number={adventure.number} unit={adventure.unit} difficulty={adventure.difficulty} maxGroupSize={adventure.maxGroupSize} minGroupSize={adventure.minGroupSize} itinerary={adventure.itinerary} img={"https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?cs=srgb&dl=pexels-francesco-ungaro-1525041.jpg&fm=jpg"} title={adventure.adventureName} host={adventure.hostId.firstName + " " + adventure.hostId.lastName} description={adventure.description}/>
+                            <FlipCard key={adventure._id} location={adventure.location} number={adventure.duration.time} unit={adventure.duration.unit} difficulty={adventure.difficulty} maxGroupSize={adventure.maxGroupSize} minGroupSize={adventure.minGroupSize} itinerary={adventure.itinerary} img={"https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?cs=srgb&dl=pexels-francesco-ungaro-1525041.jpg&fm=jpg"} title={adventure.adventureName} host={adventure.hostId.firstName + " " + adventure.hostId.lastName} description={adventure.description}/>
                         </Cell>
                             )}
                     </Gridx>
