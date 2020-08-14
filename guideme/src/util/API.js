0bb06e26573a8048dc23f0f8fd591c7ec40e2da2
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-08-14 11:00:44 -0700

    commit to merge and fix other bugs first

--- guideme/src/util/API.js
+++ guideme/src/util/API.js
@@ -25,7 +25,7 @@ export default {
     //============USERS==================
     //GET user by id
     getUserbyId: (id) => {
-        return axios.get(`${prefix}/api/user/` + id, {withCredentials:true})
+        return axios.get(`${prefix}/api/user/profile` + id, {withCredentials:true})
     },
     //POST new user
     postNewUser: (userObj) => {
