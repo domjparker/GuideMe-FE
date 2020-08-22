cfbcfe17e194e3fa3dca187ce889748a429bc120
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-08-21 16:35:07 -1000

    404 page done

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -4,17 +4,27 @@ import './style.css'
 import Wrapper from '../../components/Wrapper'
 
 
-function NotFound () {
-    
-    return(
+function NotFound() {
+
+    return (
         <>
-        <br></br>
-        <br></br>
-        <Wrapper>
-            <p>404 not found</p>
-        </Wrapper>
+            <br></br>
+            <br></br>
+            <Wrapper>
+                
+                <p className="zoom-area"> </p>
+                 <section className="error-container errorpage">
+                    <span>4</span>
+                    <span><span claclassNamess="screen-reader-text">0</span></span>
+                    <span>4</span>
+                </section>
+                <div class="link-container">
+                    <a target="blank" href="/" className="more-link">Back to the homepage</a>
+                </div>
+
+            </Wrapper>
         </>
-    )    
+    )
 }
 
 export default NotFound
