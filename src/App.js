2b7d0a6b17a55b56141f90775358324b248a79d2
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-08-22 22:05:07 -0700

    community page renders DB info and posts new feed posts

--- src/App.js
+++ src/App.js
@@ -56,6 +56,14 @@ const renderLogIn = () => {
   }
 }
 
+const renderCommunityLogin = () => {
+  if (haveData && user.loggedIn) {
+    return <Community/>
+  } else if (haveData) {
+   return <Login/>
+  }
+}
+
 
   return (
     <Router>
@@ -81,7 +89,7 @@ const renderLogIn = () => {
             <PublicProfile/>
           </Route>
           <Route exact path='/community'>
-            <Community/>
+            {renderCommunityLogin()}
           </Route>
           <Route path='*'>
             <NotFound />
--- src/pages/Community/index.js
+++ src/pages/Community/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import moment from 'moment'
 import Wrapper from '../../components/Wrapper'
 import Gridx from '../../components/Gridx'
 import Cell from '../../components/Cell'
@@ -12,117 +13,61 @@ function Community() {
     const [feed, setFeed] = useState([])
     const [post, setPost] = useState('')
     const [change, setChange] = useState(false)
+    
     useEffect(() => {
         loadFeed()
     }, [change])
 
     //make API call for feed table
     const loadFeed = async () => {
-        const feedArr= await API.getFeed()
-        console.log(feedArr)
-        const feedObj = {
-            _id: "asdfasdfasdf",
-            targetId: {
-                "_id": "5f40b62fc2e880aacc0fee58",
-                "verified": true,
-                "host": true,
-                "hostedAdventures": [],
-                "completedAdventures": [],
-                "tags": [],
-                "firstName": "maria",
-                "lastName": "maria",
-                "email": "maria@maria.com",
-                "password": "$2b$10$RB2dVKaXDr479Ag8d/rI2O7fVX2hkL3PuwXy5/oDJzXMC3QS.KvRy",
-                "mailbox": [],
-                "availability": [],
-                "__v": 0,
-                "bio": "Coolestest cool person just so cool, there is no one cooler ever on Earth. Yeah!",
-                "location": "Woods",
-                "stateLocation": "Washington",
-                "profilePictureUrl": "https://res.cloudinary.com/yestoskydiving/image/upload/v1598126251/GuideMeProfilePic/df4lzacpzyornjoyl4lj.jpg"
-            },
-            action: 'newPost',
-            adventureId: {
-                "_id": "5f40b822c2e880aacc0fee5c",
-                "tags": [
-                    "5f358b1dde3d0897c09a3986"
-                ],
-                "adventureName": "New Adventure",
-                "hostId": "5f40b62fc2e880aacc0fee58",
-                "description": "Very interesting adventure",
-                "location": "Unknown",
-                "stateLocation": "Alaska",
-                "itinerary": "Itinerary here",
-                "difficulty": "Easy",
-                "minGroupSize": 1,
-                "maxGroupSize": 1,
-                "price": 50,
-                "gearList": "No specialty gear needed",
-                "adventureImageUrl": "https://res.cloudinary.com/yestoskydiving/image/upload/v1598076961/GuideMeAdventurePic/mvnahkvtimijnmkkj89i.jpg",
-                "duration": {
-                    "time": 1,
-                    "unit": "hours"
-                },
-                "__v": 0
-            },
-            postImageUrl: '',
-            postText: 'I am so excited about all these new adventures popping up on the West coast. Cannot wait!',
-            createdAt: '8/22/2020'
-        }
-        convertFeedData(feedObj)
 
+        const {data}= await API.getFeed()
+        console.log(data)
+        let feedArr=[...data]
+        let arr=[]
+        
+        feedArr.forEach(item=>{
+            let newObj = convertFeedData(item)
+            console.log("Last object", newObj)
+            arr.push(newObj)
+            console.log('feed so far', arr)
+        })
+
+        setFeed(arr)
     }
 
     const convertFeedData = (feedObj) => {
-        console.log(feedObj.createdAt)
+        
+        let newObj={
+            userName: `${feedObj.targetId.firstName} ${feedObj.targetId.lastName}`,
+            date: moment(feedObj.createdAt).format('LLL'),
+            profilePictureUrl: feedObj.targetId.profilePictureUrl,
+            userId: feedObj.targetId._id,
+            id: feedObj._id
+        }
+        if (feedObj.targetId.stateLocation) {
+        newObj.location = `${feedObj.targetId.location}, ${feedObj.targetId.stateLocation}` 
+        } else {
+        newObj.location = '';  
+        }
         switch (feedObj.action) {
             case 'newUser':
-                setFeed([...feed, {
-                    id: feedObj._id,
-                    userId: feedObj.targetId._id,
-                    profilePictureUrl: feedObj.targetId.profilePictureUrl,
-                    location: `${feedObj.targetId.location}, ${feedObj.targetId.stateLocation}`,
-                    userName: `${feedObj.targetId.firstName} ${feedObj.targetId.lastName}`,
-                    text: ` joined GuideMe! Welcome, ${feedObj.targetId.firstName}, we wish you many story worthy adventures with us!`,
-                    date: feedObj.createdAt
-                }])
+                    newObj.text= ` joined GuideMe! Welcome, ${feedObj.targetId.firstName}, we wish you many story worthy adventures with us!`
                 break;
             case 'newGuide':
-                setFeed([...feed, {
-                    id: feedObj._id,
-                    userId: feedObj.targetId._id,
-                    profilePictureUrl: feedObj.targetId.profilePictureUrl,
-                    location: `${feedObj.targetId.location}, ${feedObj.targetId.stateLocation}`,
-                    userName: `${feedObj.targetId.firstName} ${feedObj.targetId.lastName}`,
-                    text: ` became a verified guide! Congrats, ${feedObj.targetId.firstName}, we look forward to going places with you!`,
-                    date: feedObj.createdAt
-                }])
+                newObj.text= ` became a verified guide! Congrats, ${feedObj.targetId.firstName}, we look forward to going places with you!`
                 break;
             case 'newAdventure':
-                setFeed([...feed, {
-                    id: feedObj._id,
-                    userId: feedObj.targetId._id,
-                    profilePictureUrl: feedObj.targetId.profilePictureUrl,
-                    location: `${feedObj.targetId.location}, ${feedObj.targetId.stateLocation}`,
-                    userName: `${feedObj.targetId.firstName} ${feedObj.targetId.lastName}`,
-                    text: ` published a new adventure! ${feedObj.adventureId.adventureName} sure sounds exciting!`,
-                    date: feedObj.createdAt
-                }])
+                    newObj.text= ` published a new adventure! ${feedObj.adventureId.adventureName} sure sounds exciting!`
                 break;
             case 'newReview':
                 console.log('newReview')
                 break;
             default:
-                setFeed([...feed, {
-                    id: feedObj._id,
-                    userId: feedObj.targetId._id,
-                    profilePictureUrl: feedObj.targetId.profilePictureUrl,
-                    location: `${feedObj.targetId.location}, ${feedObj.targetId.stateLocation}`,
-                    userName: `${feedObj.targetId.firstName} ${feedObj.targetId.lastName}`,
-                    text: `: ${feedObj.postText}`,
-                    date: feedObj.createdAt
-                }])
+                    newObj.text= `: ${feedObj.postText}`
+                
         }
+        return newObj
     }
 
     const handleSubmit = async () => {
@@ -135,9 +80,12 @@ function Community() {
             postImageUrl:null,
             postText:post
         }
-        console.log(postObj)
-        API.postFeed(postObj).then(res=>console.log(res)).catch(err=>console.log(err))
-        setChange(!change)
+        setPost('')
+        API.postFeed(postObj).then(res=>{
+            console.log(res)
+            setChange(!change)
+        }).catch(err=>console.log(err))
+        
     }
 
     return (
@@ -145,7 +93,7 @@ function Community() {
             <Wrapper>
                 <div className="postTextArea grid-container fluid">
                     {/* The search or host adventure form on home page */}
-                    {/* <form className="grid-container fluid"> */}
+                    
                         <Gridx classes={"grid-margin-x"}>
                             <Cell size='small-8'>
 
@@ -155,7 +103,7 @@ function Community() {
                                 <Btn onClick={handleSubmit} classes={'button searchAdventure'} text={"Post"} />
                             </Cell>
                         </Gridx>
-                    {/* </form> */}
+                    
                 </div>
                 <Gridx classes={'feedContainer grid-margin-y'}>
 
